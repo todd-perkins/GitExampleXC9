83bd661fbc799ac54a58f5dd84e3ab98a8be6202
Todd Perkins <todd@example.com>
2017-12-11 12:38:57 -0800

    Merged back into main branch

--- GitExampleXC9/ViewController.swift
+++ GitExampleXC9/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("some changes made in Todd's Branch")
+        print("some changes made in Todd's Branch - now master")
     }
 
     override var representedObject: Any? {
